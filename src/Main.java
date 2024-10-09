47dc73dab7c9708c875275f2c15812f91b10d95e
PiGru08 <grucar77@gmail.com>
2024-10-09 20:32:04 +0200

    Finalna wersja

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    Pracownik pracownikAdam = new Pracownik("Adam", "Niedziela", "Opiekun Krów", 5500);
-    Pracownik pracownikKlara = new Pracownik("Klara", "Nowa", "Obornik", 6500);
-    Farma farma1 = new Farma();
-    Zwierzeta krowa = new Zwierzeta("Adas", "Krowa", 1);
-    Wlasciciel Adam = new Wlasciciel("Adam", "Niedziela");
+        Pracownik pracownikAdam = new Pracownik("Adam", "Niedziela", "Opiekun Krów", 5500);
+        Pracownik pracownikKlara = new Pracownik("Klara", "Nowa", "Obornik", 6500);
+        Farma farma1 = new Farma();
+        Zwierzeta krowa = new Zwierzeta("Adas", "Krowa", 1);
+        Wlasciciel Adam = new Wlasciciel("Adam", "Niedziela");
         System.out.println(pracownikAdam);
         System.out.println(pracownikKlara);
         System.out.println(krowa);
         System.out.println(Adam);
         System.out.println();
+        pracownikAdam.dodajZwierzeta(krowa);
+        System.out.println(pracownikAdam.getZwierzetanaFarmie());
+        pracownikAdam.zmienPrzydzial(pracownikAdam, pracownikKlara, krowa);
+        System.out.println(pracownikAdam.getZwierzetanaFarmie());
+        System.out.println(pracownikKlara.getZwierzetanaFarmie());
     }
 }
--- src/Pracownik.java
+++ src/Pracownik.java
@@ -24,10 +24,32 @@ public class Pracownik extends Osoba {
         this.stanowisko = stanowisko;
     }
 
+    public ArrayList<Zwierzeta> getZwierzetanaFarmie() {
+        return zwierzetanaFarmie;
+    }
+
+    public void setZwierzetanaFarmie(ArrayList<Zwierzeta> zwierzetanaFarmie) {
+        this.zwierzetanaFarmie = zwierzetanaFarmie;
+    }
+
     public void dodajZwierzeta(Zwierzeta zwierzeta) {
         zwierzetanaFarmie.add(zwierzeta);
     }
+    public void zmienPrzydzial(Pracownik pracownik1,Pracownik pracownik2, Zwierzeta zwierzeDoZmiany){
+        ArrayList<Zwierzeta> lista= pracownik1.getZwierzetanaFarmie();
+        ArrayList<Zwierzeta> lista2= pracownik2.getZwierzetanaFarmie();
+        for (int i = 0; i < lista.size(); i++) {
+            if(lista.contains(zwierzeDoZmiany)){
+                Zwierzeta zwierzeDoDodania = lista.get(i);
+                lista.remove(i);
+                pracownik1.setZwierzetanaFarmie(lista);
+                lista2.add(zwierzeDoDodania);
+                pracownik2.setZwierzetanaFarmie(lista2);
+                break;
+            }
+        }
 
+    }
     @Override
     public String toString() {
         return "Pracownik{" +
